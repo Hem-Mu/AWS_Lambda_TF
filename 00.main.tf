f0a8d1481945dd3bc54dd8b0f164e6aa710323b4
unknown <kimmim1223@naver.com>
2022-06-03 15:50:26 +0900

    2022-06-03

--- 00.main.tf
+++ 00.main.tf
@@ -3,3 +3,30 @@ provider "aws" {
   secret_key = var.secret_key
   region = "ap-northeast-3"
 }
+data "archive_file" "lambda" {
+  type = "zip"
+
+  source_dir  = "${path.module}/lambda" # 압축 할 local directory name
+  output_path = "${path.module}/lambda.zip"
+} 
+
+resource "aws_s3_bucket" "lambda_s3" {
+  bucket = "hemmu-lambda-s3"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "lambda"
+  }
+} # lambda s3
+
+resource "aws_s3_bucket_acl" "example" {
+  bucket = aws_s3_bucket.lambda_s3.id
+  acl    = "private"
+}
+
+resource "aws_s3_object" "lambda" {
+  bucket = aws_s3_bucket.lambda_s3.id
+
+  key    = "lambda.zip"
+  source = data.archive_file.lambda.output_path
+}
