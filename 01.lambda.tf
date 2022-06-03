431e39b06837c349e1afcbf80e50e0ed657b9b96
unknown <kimmim1223@naver.com>
2022-06-03 16:26:22 +0900

    2022-06-03

--- 01.lambda.tf
+++ 01.lambda.tf
@@ -2,20 +2,18 @@ resource "aws_lambda_function" "test_lambda" {
   # If the file is not in the current working directory you will need to include a 
   # path.module in the filename.
   filename      = data.archive_file.lambda.output_path # 압축 한 data file
-  function_name = "lambda_function_name_test"
+  function_name = "EC2-Scheduler"
   role          = aws_iam_role.iam_for_lambda.arn
-  handler       = "index.test" # 코드 함수의 진입점
+  handler       = "lambda.lambda_handler" # Python Handler Name ex) Python_File_Name.Method_Name
+  timeout = 30
 
-  # The filebase64sha256() function is available in Terraform 0.11.12 and later
-  # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
-  # source_code_hash = "${base64sha256(file("lambda_function_payload.zip"))}"
   source_code_hash = data.archive_file.lambda.output_base64sha256
 
   runtime = "python3.8"
 
-  environment {
+  /* environment {
     variables = {
       foo = "bar"
     }
-  }
+  } */
 }
